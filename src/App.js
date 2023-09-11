6087430f85f5d42f186d4ffec230e4ac0ccbd8b1
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-09-11 16:20:55 +0330

    Install & config tailwind css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-const App = ()=>{
-  return "hi";
-}
+import './index.css';
+
+const App = () => (
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+);
 
 export default App;
