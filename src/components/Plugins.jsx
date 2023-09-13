7c3a6e43482e72e0b16c75499860f63e58f1295d
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-09-13 18:02:30 +0330

    Add plugins componet and initial style it

---
+++ src/components/Plugins.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Plugin from './Plugin';
+
+const Plugins = ({ plugins, searchValue }) => {
+  if (!searchValue) {
+    return (
+      <div className="container mx-auto my-6 grid grid-cols-3 gap-4 grid-auto-flow">
+        { plugins.map((plugin) => (
+          <Plugin
+            key={plugin.id}
+            icon={plugin.icon}
+            title={plugin.title}
+            author={plugin.author}
+            description={plugin.description}
+            repo={`https://github.com/${plugin.repo}`}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  const filteredPlugins = plugins.filter(
+    (plugin) => plugin.title.toLowerCase().includes(searchValue.toLowerCase()),
+  );
+
+  return (
+    <div className="container mx-auto my-6 grid grid-cols-3 gap-4 grid-auto-flow">
+      { filteredPlugins.map((plugin) => (
+        <Plugin
+          key={plugin.id}
+          icon={plugin.icon}
+          title={plugin.title}
+          author={plugin.author}
+          description={plugin.description}
+          repo={`https://github.com/${plugin.repo}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+Plugins.propTypes = {
+  plugins: PropTypes.string.isRequired,
+  searchValue: PropTypes.string.isRequired,
+};
+
+export default Plugins;
