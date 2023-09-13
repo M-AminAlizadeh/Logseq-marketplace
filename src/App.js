ccd3c1fb56ae184ed0e2daabbda54de03d6d787a
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-09-13 18:06:26 +0330

    Add routing system to the app using react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Plugins from './pages/PluginsPage';
+import Themes from './pages/ThemesPage';
+import Navbar from './components/Navbar';
 import './index.css';
 
-const App = () => 'app';
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Plugins />} />
+      <Route path="/themes" element={<Themes />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
