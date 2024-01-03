f06f5f84f9f2b432b3253c0f666d3ab46be39ea0
m-aminalizadeh <aminalizadeh@riseup.net>
2024-01-03 13:58:16 +0330

    Add searchbox component and exchange search value between it and ThemesPage component

---
+++ src/components/ThemesSearchbox.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const ThemesSearchbox = ({ handleSearch }) => (
+  <input
+    className="border-2 rounded-md p-2"
+    type="text"
+    placeholder="Search Themes..."
+    onChange={(e) => { handleSearch(e); }}
+    title="Search base on name of the theme"
+  />
+);
+
+ThemesSearchbox.propTypes = {
+  handleSearch: PropTypes.string.isRequired,
+};
+
+export default ThemesSearchbox;
