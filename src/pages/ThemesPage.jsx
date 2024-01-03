0d69d1e0a153552864359653f558cc1b36ebecba
m-aminalizadeh <aminalizadeh@riseup.net>
2024-01-03 13:39:17 +0330

    Add fetchdata and render the themes and add searchbox functionality

--- src/pages/ThemesPage.jsx
+++ src/pages/ThemesPage.jsx
@@ -1,3 +1,36 @@
-const Themes = () => 'Themes page';
+import { useState, useEffect } from 'react';
+import Themes from '../components/Themes';
+import ThemesSearchbox from '../components/ThemesSearchbox';
 
-export default Themes;
+const ThemesPage = () => {
+  const [themes, setThemes] = useState([]);
+  const [searchValue, setSearchValue] = useState();
+
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://cdn.jsdelivr.net/gh/logseq/marketplace@master/plugins.json');
+      if (response.ok) {
+        const data = await response.json();
+        const { packages } = data;
+        setThemes(packages.filter((item) => item.theme));
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container mx-auto my-6">
+      <div className="flex justify-between">
+        <h2 className="text-3xl font-bold">Themes</h2>
+        <ThemesSearchbox handleSearch={handleSearch} />
+      </div>
+      <Themes themes={themes} searchValue={searchValue} />
+    </div>
+  );
+};
+
+export default ThemesPage;
