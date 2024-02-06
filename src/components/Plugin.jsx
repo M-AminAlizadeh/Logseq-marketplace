606b1a664f2bfc65d79cd55b5765fd548c2d4d16
m-aminalizadeh <aminalizadeh@riseup.net>
2024-02-06 11:55:05 +0330

    Change the layout of the plugin cards

--- src/components/Plugin.jsx
+++ src/components/Plugin.jsx
@@ -14,27 +14,43 @@ const Plugin = ({
     icon ? `https://raw.githubusercontent.com/logseq/marketplace/master/packages/${id}/${properIconUrl(icon)}` : 'https://img.icons8.com/pastel-glyph/64/page-not-found--v2.png'
   );
 
+  const properDescription = (description) => {
+    const maxContentLength = 40;
+    if (description.length <= maxContentLength) {
+      return description;
+    }
+    return `${description.slice(0, maxContentLength)} ...`;
+  };
+
   return (
-    <section className="border-2 p-3 rounded-md">
-      <img
-        width="100px"
-        src={getIconPath(icon)}
-        alt="plugin-icon"
-        className="my-3"
-      />
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl" title="Plugin's Name">{title}</h1>
-        <span title="Auhtor Username">{author}</span>
+    <section className="flex flex-col gap-3  border-2 p-2 rounded-md">
+      <div className="flex items-center relative">
+        <img
+          width="75px"
+          src={getIconPath(icon)}
+          alt="plugin-icon"
+          className="my-3"
+        />
+        <div className="flex flex-col justify-between mx-3">
+          <h2 className="text-xl" title="Plugin's Name">{title}</h2>
+          <p className="py-2" title="Description">{properDescription(description)}</p>
+        </div>
+        <a
+          href={repo}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute top-2 right-2"
+        >
+          <img width="25" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/external-github-with-cat-logo-an-online-community-for-software-development-logo-bold-tal-revivo.png" alt="external-github-with-cat-logo-an-online-community-for-software-development-logo-bold-tal-revivo" />
+        </a>
       </div>
-      <p className="py-2" title="Description">{description}</p>
-      <a
-        href={repo}
-        className="font-bold text-cyan-500 hover:border-b-cyan-500 hover:border-b-2"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Repo Link
-      </a>
+
+      <div className="flex justify-between items-center">
+        <button type="button" className="border px-3 py-2 rounded-xl hover:bg-slate-600 hover:text-white">
+          More
+        </button>
+        <span title="Auhtor's name">{author}</span>
+      </div>
     </section>
   );
 };
