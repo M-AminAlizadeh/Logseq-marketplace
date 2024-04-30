f5bba1128a2e84c0afffbaed17df95f09960cd49
m-aminalizadeh <aminalizadeh@riseup.net>
2024-04-30 15:36:10 +0330

    Fetch the readme file of the repo that has been clicked and show the markdown format of it in the popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,21 +2,45 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const Popup = ({ pluginClickedID, setPopup }) => {
-  const [descriptionContent, setDescriptionContent] = useState('Loading');
+  const [repoUrl, setRepoUrl] = useState('');
+  const [readmeContent, setReadmeContent] = useState('Loading ...');
+  const branchNameList = ['main', 'master'];
+
   useEffect(() => {
+    const readmeFetch = async () => {
+      const promises = branchNameList.map(async (branchName) => {
+        const response = await fetch(`https://raw.githubusercontent.com/${repoUrl}/${branchName}/README.md`);
+        if (response.status === 200) {
+          const data = await response.text();
+          if (data) {
+            setReadmeContent(data);
+            return true;
+          }
+        }
+        return false;
+      });
+
+      const results = await Promise.all(promises);
+      if (!results.some((found) => found)) {
+        console.error('README not found in any branch');
+      }
+    };
+
     const fetchData = async () => {
       fetch('https://cdn.jsdelivr.net/gh/logseq/marketplace@master/plugins.json')
         .then((response) => response.json())
         .then((data) => {
           data.packages.forEach((item) => {
             if (!item.theme && item.id === pluginClickedID) {
-              setDescriptionContent(item.description);
+              setRepoUrl(item.repo);
+              readmeFetch();
             }
           });
         });
     };
     fetchData();
-  }, [pluginClickedID]);
+  }, [pluginClickedID, repoUrl]);
+
   return (
     <div className="border bg-white w-10/12 p-5 mx-auto my-5 h-screen relative z-10">
       <div className="flex justify-between">
@@ -26,7 +50,7 @@ const Popup = ({ pluginClickedID, setPopup }) => {
         </button>
       </div>
       <article className="my-5">
-        {descriptionContent}
+        {readmeContent}
       </article>
     </div>
   );
